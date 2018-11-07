3e1088815defb6a6e156ec3435813db36ba35b1f
Sylvain H Yao <sylvain.h.yao@cnsdc01.pwc.com>
2018-11-07 19:15:42 +0800

    [BUF]webpack fix

--- src/components/navbar/YBNavbar.tsx
+++ src/components/navbar/YBNavbar.tsx
@@ -6,7 +6,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Toolbar from "@material-ui/core/Toolbar";
-import { List } from "@material-ui/icons";
+import List from "@material-ui/icons/List";
 // import Typography from "@material-ui/core/Typography";
 import { Theme } from "@material-ui/core/styles/createMuiTheme";
 import createStyles from "@material-ui/core/styles/createStyles";
